e47ae235d4afbd5649b2bc1bce27183845bc7611
Kylie Banman <339780520@GoTVDSB.ca>
2025-03-04 14:25:02 -0500

    Add project files.

---
+++ Topic 7.2 assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_7._2_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
