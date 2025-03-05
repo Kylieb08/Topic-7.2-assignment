cce4dec2cab8f23b5303c6d7eaf99a06049608d1
Kylie Banman <339780520@GoTVDSB.ca>
2025-03-05 13:48:35 -0500

    finished assignment one

--- Topic 7.2 assignment/Program.cs
+++ Topic 7.2 assignment/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Assignment one
+            Console.WriteLine("Assignment one");
+            Console.WriteLine();
+
+            for (int i = 10; i > -1; i = i - 1)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(i);
+            }
+            Thread.Sleep(1000);
+            Console.WriteLine("Blast off!!");
         }
     }
 }
