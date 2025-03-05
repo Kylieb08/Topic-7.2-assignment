cf5db321cac14071728953302dda7c634f76cb05
Kylie Banman <339780520@GoTVDSB.ca>
2025-03-05 14:36:25 -0500

    started assignment three

--- Topic 7.2 assignment/Program.cs
+++ Topic 7.2 assignment/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
 
+            string name;
+
             //Assignment one
             //Console.WriteLine("Assignment one");
             //Console.WriteLine();
@@ -18,18 +20,33 @@
             //Console.WriteLine("Blast off!");
             //Console.ReadLine();
 
-            //Assignment two
-            Console.WriteLine("Assignment two");
-            Console.WriteLine();
-            Thread.Sleep(1000);
+            ////Assignment two
+            //Console.WriteLine("Assignment two");
+            //Console.WriteLine();
+            //Thread.Sleep(1000);
 
-            Console.WriteLine("X \t Y");
-            Console.WriteLine("----------");
-            for (int x = -10; x < 12; x = x + 2)
+            //Console.WriteLine("X \t Y");
+            //Console.WriteLine("----------");
+            //for (int x = -10; x < 12; x = x + 2)
+            //{
+            //    Thread.Sleep(1000);
+            //    int y = (int)Math.Pow(x, 2);
+            //    Console.WriteLine(x + "\t" + y);
+            //}
+            //Console.ReadLine();
+
+            //Assignment three
+            Console.WriteLine("Assignment three");
+            Console.WriteLine();
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine();
+            for (int i = 0; i < 10; i = i + 1)
             {
                 Thread.Sleep(1000);
-                int y = (int)Math.Pow(x, 2);
-                Console.WriteLine(x + "\t" + y);
+                Console.WriteLine(name);
             }
         }
     }
