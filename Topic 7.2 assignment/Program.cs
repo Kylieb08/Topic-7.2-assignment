a0365909e2a681549f3f0ae2069298c8a1821f33
Kylie Banman <339780520@GoTVDSB.ca>
2025-03-05 14:07:37 -0500

    almost finished task two, just need tabs

--- Topic 7.2 assignment/Program.cs
+++ Topic 7.2 assignment/Program.cs
@@ -6,16 +6,29 @@
         {
 
             //Assignment one
-            Console.WriteLine("Assignment one");
-            Console.WriteLine();
+            //Console.WriteLine("Assignment one");
+            //Console.WriteLine();
 
-            for (int i = 10; i > -1; i = i - 1)
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine(i);
-            }
+            //for (int i = 10; i > -1; i = i - 1)
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine(i);
+            //}
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Blast off!");
+            //Console.ReadLine();
+
+            //Assignment two
+            Console.WriteLine("Assignment two");
+            Console.WriteLine();
             Thread.Sleep(1000);
-            Console.WriteLine("Blast off!!");
+
+            for (int x = -10; x < 12; x = x + 2)
+            {
+                int y = (int)Math.Pow(x, 2);
+                Console.WriteLine(x + " " + y);
+                Thread.Sleep(1000);
+            }
         }
     }
 }
