d735fd5b2d4f1bb663f14bc70f71990e6b6e8b8e
Kylie Banman <339780520@GoTVDSB.ca>
2025-03-06 13:56:57 -0500

    done assignments

--- Topic 7.2 assignment/Program.cs
+++ Topic 7.2 assignment/Program.cs
@@ -6,34 +6,43 @@
         {
 
             string name;
+            int nameRepeat = 10;
 
             //Assignment one
-            //Console.WriteLine("Assignment one");
-            //Console.WriteLine();
+            Console.WriteLine("Assignment one");
+            Console.WriteLine();
 
-            //for (int i = 10; i > -1; i = i - 1)
-            //{
-            //    Thread.Sleep(1000);
-            //    Console.WriteLine(i);
-            //}
-            //Thread.Sleep(1000);
-            //Console.WriteLine("Blast off!");
-            //Console.ReadLine();
+            for (int i = 10; i > -1; i = i - 1)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(i);
+            }
+            Thread.Sleep(1000);
+            Console.WriteLine("Blast off!");
+            Thread.Sleep(1000);
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
+            Console.Clear();
 
-            ////Assignment two
-            //Console.WriteLine("Assignment two");
-            //Console.WriteLine();
-            //Thread.Sleep(1000);
+            //Assignment two
+            Console.WriteLine("Assignment two");
+            Console.WriteLine();
+            Thread.Sleep(1000);
 
-            //Console.WriteLine("X \t Y");
-            //Console.WriteLine("----------");
-            //for (int x = -10; x < 12; x = x + 2)
-            //{
-            //    Thread.Sleep(1000);
-            //    int y = (int)Math.Pow(x, 2);
-            //    Console.WriteLine(x + "\t" + y);
-            //}
-            //Console.ReadLine();
+            Console.WriteLine("X \t Y");
+            Console.WriteLine("----------");
+            for (int x = -10; x < 12; x = x + 2)
+            {
+                Thread.Sleep(1000);
+                int y = (int)Math.Pow(x, 2);
+                Console.WriteLine(x + "\t" + y);
+            }
+            Thread.Sleep(1000);
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
+            Console.Clear();
 
             //Assignment three
             Console.WriteLine("Assignment three");
@@ -43,10 +52,16 @@
             name = Console.ReadLine();
 
             Console.WriteLine();
-            for (int i = 0; i < 10; i = i + 1)
+            Thread.Sleep(1000);
+            if (name.ToLower() == "aldworth")
             {
-                Thread.Sleep(1000);
+                nameRepeat = 5;
+            }
+
+            for (int i = 0; i < nameRepeat; i = i + 1)
+            {
                 Console.WriteLine(name);
+                Thread.Sleep(1000);
             }
         }
     }
